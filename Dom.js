5af5246d85026b96ec22b05c80fd60a197314b87
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-02-16 19:31:29 +0530

    Sharpener Task4

--- Dom.js
+++ Dom.js
@@ -13,8 +13,19 @@ var HeaderTitle=document.getElementById('header-title');
 
  //GETELEMENTBYCLASSNAME
 
-FormTitle=document.getElementById('forms');
+var FormTitle=document.getElementById('forms');
 
 FormTitle.style.fontWeight='bold';
 FormTitle.style.color='green';
+
+var Items=document.getElementsByClassName('list-group-item');
+console.log(Items);
+Items[2].style.backgroundColor='lightgreen';
+
+
+for( var i=0;i<Items.length;i++)
+ {
+     Items[i].style.fontWeight='bold';
+ }
+
 
