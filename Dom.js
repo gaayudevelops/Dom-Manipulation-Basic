b35c2b7290b4da605dd7d704527cb0bd5930f61b
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-02-16 19:25:45 +0530

    Sharpener Task 3

---
+++ Dom.js
@@ -0,0 +1,20 @@
+//GETELEMENTBYID
+
+var HeaderTitle=document.getElementById('header-title');
+
+ HeaderTitle.textContent='Item Display';
+ HeaderTitle.innerText="Item Lister";
+ HeaderTitle.style.color="white";
+ HeaderTitle.style.fontFamily='segoe ui';
+ HeaderTitle.innerHTML="<h1> Display Items</h1>";
+
+ var Border=document.getElementById('main-header');
+ Border.style.borderBottom='solid 5px black';
+
+ //GETELEMENTBYCLASSNAME
+
+FormTitle=document.getElementById('forms');
+
+FormTitle.style.fontWeight='bold';
+FormTitle.style.color='green';
+
