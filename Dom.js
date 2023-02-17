4686aaa5760edbc68585c928a15ee3b0b964b169
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-02-17 13:26:34 +0530

    Add files via upload

--- Dom.js
+++ Dom.js
@@ -6,7 +6,7 @@ var HeaderTitle=document.getElementById('header-title');
  HeaderTitle.innerText="Item Lister";
  HeaderTitle.style.color="white";
  HeaderTitle.style.fontFamily='segoe ui';
- HeaderTitle.innerHTML="<h1> Display Items</h1>";
+
 
  var Border=document.getElementById('main-header');
  Border.style.borderBottom='solid 5px black';
@@ -48,3 +48,80 @@ var Items=document.getElementsByClassName('list-group-item');
 
     var lastelem=document.querySelector('.list-group-item:nth-child(3)') 
     lastelem.style.display=" none"; 
+
+ var itemlist=document.querySelector('#items');
+
+// firstChild
+console.log(itemlist.firstChild);
+
+//FirstElement Child
+console.log(itemlist.firstElementChild);
+
+//lastChild
+console.log(itemlist.lastChild);
+
+//LastElement Child
+console.log(itemlist.lastElementChild);
+
+//nextSibling
+console.log(itemlist.nextSibling);
+
+//nextElementSibling
+console.log(itemlist.nextElementSibling);
+
+//previousSibling
+console.log(itemlist.previousSibling);
+
+//previousElementSibling
+console.log(itemlist.previousElementSibling);
+
+//Create Element
+var newDiv=document.createElement('div');
+
+//adding classname 
+newDiv.className='hello';
+
+//adding id
+newDiv.id='hello 1';
+
+//adding attribute
+newDiv.setAttribute('title','Hello Div');
+
+//creating text node
+var newDivText=document.createTextNode('Hello');
+console.log(newDiv);
+
+//add text to Div
+newDiv.appendChild(newDivText);
+
+
+//to insert new element from script to html
+
+    //to insert newDiv before item lister
+var container=document.querySelector('header .container');
+
+var h1= document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+
+
+  //to insert newli hello world before first item 
+
+//Create Element
+var newli=document.createElement('li');
+
+//adding classname
+newli.className='list-group-item';
+
+//creating textnode
+var newliText=document.createTextNode('HELLO');
+
+//adding text to Div
+newli.appendChild(newliText);
+
+var item=document.querySelector('.list-group');
+
+var li=item.children[0];
+
+item.insertBefore(newli,li);
